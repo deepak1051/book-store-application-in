237b807a85e1aef2e2b3b063f47a9e07c2526825
deepak1051 <deepakpanwar1051@gmail.com>
2024-11-14 01:05:17 +0530

    build cmds

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import bookRoutes from './routes/book.routes.js';
 
+import path from 'node:path';
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('connected to db'))
@@ -22,6 +24,20 @@ app.get('/ping', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
